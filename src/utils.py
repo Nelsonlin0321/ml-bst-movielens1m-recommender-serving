8daf4fd70cf7845493ec9e2bdcb60fd4ef0852aa
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-08-12 15:32:26 +0800

    log download s3

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,10 @@ import os
 import json
 import subprocess
 import time
+from tqdm import tqdm
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def save_json(json_object, file_path):
     with open(file_path, mode='w', encoding='utf-8') as f:
@@ -42,6 +46,9 @@ def download_s3_directory(s3_dir_path):
 
     if output.returncode==1:
         raise Exception(output.stderr.decode("utf-8"))
+    else:
+        logger.info(output.stdout.decode("utf-8"))
+        
     
     return os.path.abspath("./")
 
@@ -55,3 +62,29 @@ def timer(func):
         print(f"Runtime of {func.__name__}: {runtime} ms")
         return result
     return wrapper
+
+def recursively_listdir(dir_path, extensions=[''], with_tqdm=False):
+
+    dir_path = os.path.abspath(dir_path)
+    if not os.path.isdir(dir_path):
+        return [dir_path]
+
+    file_list = []
+    file_paths = listdir_with_full_path(dir_path)
+    if with_tqdm:
+        file_paths = tqdm(file_paths, position=0, leave=True,
+                          desc=f"INFO: Scaning files under the folder {dir_path}")
+    for file_path in file_paths:
+        if os.path.isdir(file_path):
+            file_list_under_dir = recursively_listdir(
+                file_path, extensions, False)
+            file_list.extend(file_list_under_dir)
+        else:
+            for ext in extensions:
+                if file_path.endswith(ext):
+                    file_list.append(file_path)
+                    break
+    return file_list
+
+def listdir_with_full_path(dir_path):
+    return [os.path.join(dir_path, path) for path in os.listdir(dir_path)]
