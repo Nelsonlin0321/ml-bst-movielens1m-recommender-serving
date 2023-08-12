a80f0a6cd7560be4581e3efb5a0d82af8a8b500c
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-08-12 20:07:41 +0800

    add intergration tet

---
+++ integration_test/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{
+    "resource": "/healthcheck",
+    "path": "/healthcheck",
+    "httpMethod": "GET",
+    "requestContext": {
+    },
+    "isBase64Encoded": false
+}'
+
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{
+    "resource": "/recommend",
+    "path": "/recommend",
+    "httpMethod": "POST",
+    "requestContext": {
+        "resourcePath": "/recommend",
+        "httpMethod": "POST"
+    },
+    "body": "{\"movie_ids\": [1, 2, 3, 4], \"user_age\": 23, \"sex\": \"M\", \"topk\": 1}",
+    "isBase64Encoded": false
+}'
+
+
---
+++ integration_test/test_lambda.py
@@ -0,0 +1,25 @@
+import os
+import boto3
+import dotenv
+REGION = os.getenv("AWS_DEFAULT_REGION", "ap-southeast-1")
+dotenv.load_dotenv("./../.env")
+
+session = boto3.Session(region_name=REGION)
+
+lambda_client = session.client('lambda')
+
+with open("./integration_test/test_healthcheck_payload.json", mode='r', encoding='utf-8') as f:
+    payload = f.read()
+
+response = lambda_client.invoke(
+    FunctionName='movielens1m-recommender-lambda',
+    Payload=payload
+)
+
+with open("./integration_test/test_recommend_payload.json", mode='r', encoding='utf-8') as f:
+    payload = f.read()
+
+response = lambda_client.invoke(
+    FunctionName='movielens1m-recommender-lambda',
+    Payload=payload
+)
