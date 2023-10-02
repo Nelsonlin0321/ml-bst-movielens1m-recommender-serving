f1bc9f130de1d165dea840e9c31e043dc3cba78c
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-10-03 00:28:21 +0800

    print response

--- integration_test/test_lambda.py
+++ integration_test/test_lambda.py
@@ -18,6 +18,7 @@ response = lambda_client.invoke(
     FunctionName=FunctionName,
     Payload=payload
 )
+print(response['Payload'].read())
 
 with open("./integration_test/test_recommend_payload.json", mode='r', encoding='utf-8') as f:
     payload = f.read()
@@ -26,3 +27,5 @@ response = lambda_client.invoke(
     FunctionName=FunctionName,
     Payload=payload
 )
+
+print(response['Payload'].read())
